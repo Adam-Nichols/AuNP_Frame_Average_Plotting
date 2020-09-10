Made the following improvements: + Works for any file count + Works for any Au count + Averaging function rounds correctly + Tiles of Au in csv corrected to include (1/3) factorMade the following improvements: + Works for any file count + Works for any Au count + Averaging function rounds correctly + Tiles of Au in csv corrected to include (1/3) factor

Frame_Average_Plotter.py
@@ -11,7 +11,7 @@ plot_atoms = ['Au', 'Water', 'PEG']  # selection of atoms below
 # Full atom list: ['Cr', 'NR', 'OR', 'SP', 'HCM', 'HNR', 'OT', 'HT', 'Au', 'Water', 'PEG']
 # Individual plot type list for fast copy/paste: ['Cr', 'NR', 'OR', 'SP', 'Au', 'Water']
 # Combined plot type list for fast copy/paste: ['Au', 'Water', 'PEG']
-chdir(r"C:\MBN\Au135_08PEG2_Water_equil_frames")
+chdir(r"C:\MBN\Au135_08PEG2_Water_equil_frames_test")
 
 
 read_files = glob.glob("*.xyz")
@@ -19,7 +19,7 @@ file_count = len(read_files)
 
 complete_data = []
 
-for i in range(len(read_files)):
+for i in range(file_count):
     data_sheet = open(read_files[i], "r")
     data_sheet = data_sheet.read().split()
     data_sheet = data_sheet[4:]
@@ -29,6 +29,8 @@ raw_positions = []
 for sublist in complete_data:
     for item in sublist:
         raw_positions.append(item)
+
+au_count = int(raw_positions.count("Au")*(1/file_count))
 
 y_labels = []
 radius = []
@@ -49,20 +51,20 @@ data = data.astype({"x": float, "y": float, "z": float})
 atom_types = list(data.index.unique())
 
 row_count = data.shape[0]
-row_count = int(row_count*0.1)
+row_count = int(row_count*(1/file_count))
+
 
 for i in range(file_count):
-    au_data = data.iloc[(i+1)*row_count-135:(i+1)*row_count, :]
+    au_data = data.iloc[(i+1)*row_count-au_count:(i+1)*row_count, :]
     x_avg = au_data.mean()[0]
     y_avg = au_data.mean()[1]
     z_avg = au_data.mean()[2]
     data["x"][i*row_count:i*row_count+row_count] -= x_avg
-    data["y"][i * row_count:i * row_count + row_count] -= y_avg
-    data["z"][i * row_count:i * row_count + row_count] -= z_avg
+    data["y"][i*row_count:i*row_count+row_count] -= y_avg
+    data["z"][i*row_count:i*row_count+row_count] -= z_avg
 
 data["Distance"] = np.sqrt(data["x"]**2 + data["y"]**2 + data["z"]**2)
-data["Shell"] = data["Distance"]
-data["Shell"] = data["Shell"].apply(np.floor)
+data["Shell"] = data["Distance"].apply(np.floor)
 data = data.astype({"Shell": int})
 
 density = pd.DataFrame()
@@ -78,10 +80,13 @@ for i in range(plot_radius):
     for f in atom_types:
         try:
             series = data.loc[[f], ["Shell"]] == i
-            count = int(series.Shell.value_counts()[1])
-            density.at[i+1, f] = count*(1/file_count)
+            count = series.Shell.value_counts()[1]
+            density.at[i+1, f] = count
         except KeyError:
             pass
+
+for f in atom_types:
+    density[f] = density[f]*(1/file_count)
 
 density["Water"] = (density["OT"] + density["HT"])*(1/3)
 atom_types.append("Water")
@@ -94,8 +99,6 @@ density["Au"] = density["Au"]*(1/3)
 
 for f in atom_types:
     density[f+"_rho"] = density[f]/density["Volume"]
-
-density.to_csv("Frame_Averaged_Densities.csv")
 
 x_data = range(len(density))
 for f in plot_atoms:
@@ -110,3 +113,6 @@ plt.xlabel("Radius (Angstroms)")
 plt.ylabel("Number Density")
 plt.hlines(0.0334, 0, len(density), colors="gray", linestyles="dashed")
 plt.show()
+
+density.rename(columns={"Au": "Au_(1/3)", "Au_rho": "Au_(1/3)_rho"})
+density.to_csv("Frame_Averaged_Densities.csv")